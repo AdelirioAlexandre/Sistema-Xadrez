128850309bd9a9744183346008e23120700f97b7
Adelirio alexandre <adelirioalexandre03@gmail.com>
2023-07-06 01:24:21 -0300

    Finalizado a classe: Posicao

---
+++ src/Aplicacao/Principal.java
@@ -0,0 +1,9 @@
+package Aplicacao;
+import JogoTabuleiro.Posicao;
+public class Principal{
+    public static void main(String[] args) {
+        
+        Posicao pos = new Posicao(3, 5);
+        System.out.println(pos);
+    }
+}
