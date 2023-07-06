a3dd2ac1001cb21054f74c12800b12675def15c6
Adelirio alexandre <adelirioalexandre03@gmail.com>
2023-07-06 02:52:44 -0300

    Refiz o projeto toda na versão ingles pois o algoritmo estava dando erro, nessa versão infelizmente o projeto não esta executando sendo que a forma que foi feito esta correto

--- src/Aplicacao/Principal.java
+++
@@ -1,9 +0,0 @@
-package Aplicacao;
-import JogoTabuleiro.Posicao;
-public class Principal{
-    public static void main(String[] args) {
-        
-        Posicao pos = new Posicao(3, 5);
-        System.out.println(pos);
-    }
-}
---
+++ src/application/Program.java
@@ -0,0 +1,9 @@
+package application;
+import chess.ChessMatch;
+public class Program{
+    public static void main(String[] args) {
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+        
+    }
+}
