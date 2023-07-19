7f75126b488a43031b70287604aa99d2d69959da
Adelirio alexandre <adelirioalexandre03@gmail.com>
2023-07-19 09:23:12 -0300

    Implementado movimentos possiveis para o Rei

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,5 +1,6 @@
-package chessPieces;
+package chess.pieces;
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 public class King extends ChessPiece {
@@ -11,6 +12,10 @@ public class King extends ChessPiece {
 	@Override
 	public String toString() {
 		return "K";
+	}
+
+	private boolean canMove(Position position){
+		ChessPiece p = (ChessPiece).getBoard().piece(position);	
 	}
 
 	@Override
