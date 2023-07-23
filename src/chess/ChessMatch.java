bbb9240ff08925322a553a08be53d2197d1ac23d
Adelirio alexandre <adelirioalexandre03@gmail.com>
2023-07-23 18:05:45 -0300

    Jogada especial Torre Finalizado

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -92,6 +92,22 @@ public class ChessMatch {
 			piecesOnTheBoard.remove(capturedPiece);
 			capturedPieces.add(capturedPiece);
 		}
+
+		if(p instanceof King && target.getColumn() == source.getColumn() +2){
+			Position sourceT = new Position(source.getRow(), source.getColumn() +3);
+			Position targetT = new Position(source.getRow(), source.getColumn() +1);
+			ChessPiece rook = (ChessPiece)board.removePiece(sourceT);
+			board.placePiece(rook, targetT);
+			rook.increaseMoveCount();
+		}
+
+		if(p instanceof King && target.getColumn() == source.getColumn() -2){
+			Position sourceT = new Position(source.getRow(), source.getColumn() -4);
+			Position targetT = new Position(source.getRow(), source.getColumn() -1);
+			ChessPiece rook = (ChessPiece)board.removePiece(sourceT);
+			board.placePiece(rook, targetT);
+			rook.increaseMoveCount();
+		}
 		
 		return capturedPiece;
 	}
@@ -105,6 +121,22 @@ public class ChessMatch {
 			board.placePiece(capturedPiece, target);
 			capturedPieces.remove(capturedPiece);
 			piecesOnTheBoard.add(capturedPiece);
+		}
+
+		if(p instanceof King && target.getColumn() == source.getColumn() +2){
+			Position sourceT = new Position(source.getRow(), source.getColumn() +3);
+			Position targetT = new Position(source.getRow(), source.getColumn() +1);
+			ChessPiece rook = (ChessPiece)board.removePiece(targetT);
+			board.placePiece(rook, sourceT);
+			rook.decreaseMoveCount();
+		}
+
+		if(p instanceof King && target.getColumn() == source.getColumn() -2){
+			Position sourceT = new Position(source.getRow(), source.getColumn() -4);
+			Position targetT = new Position(source.getRow(), source.getColumn() -1);
+			ChessPiece rook = (ChessPiece)board.removePiece(targetT);
+			board.placePiece(rook, sourceT);
+			rook.decreaseMoveCount();
 		}
 	}
 	
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -92,8 +92,8 @@ public class King extends ChessPiece {
 				Position p1 = new Position(position.getRow(), position.getColumn() -1);
 				Position p2 = new Position(position.getRow(), position.getColumn() -2);
 				Position p3 = new Position(position.getRow(), position.getColumn() -3);
-				if(getBoard().piece(p1) == null && getBoard().piece(p2) == null){
-					mat[position.getRow()][position.getColumn() +2] = true;
+				if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null){
+					mat[position.getRow()][position.getColumn() -2] = true;
 				}
 			}
 		}
